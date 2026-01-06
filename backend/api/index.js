b320e91ab5355b0b757bc199ea47467574b74fb8
andremillwood <andremillwood@gmail.com>
2026-01-05 21:25:41 -0500

    fix: remove unused body-parser dependency causing backend crash

--- backend/api/index.js
+++ backend/api/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const helmet = require('helmet');
-const bodyParser = require('body-parser');
 const { requireAuth } = require('../middleware/auth');
 require('dotenv').config();
 
