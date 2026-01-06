e304945958b0391a7969fcef4bafed456cf06f69
andremillwood <andremillwood@gmail.com>
2026-01-06 02:46:04 -0500

    fix: Remove body-parser require (package not installed)
    - body-parser was removed in earlier commit but revert brought it back
    - This require statement caused MODULE_NOT_FOUND crash

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const bodyParser = require('body-parser');
 const { requireAuth } = require('../middleware/auth');
 require('dotenv').config();
 
