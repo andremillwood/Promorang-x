04ea4b196e80043cfde62d604a0cc7c42eb7078d
andremillwood <andremillwood@gmail.com>
2026-08-24 13:35:23 -0500

    feat: mount consumer React preview behind query flag

--- apps/web/src/pages/Index.tsx
+++ apps/web/src/pages/Index.tsx
@@ -1,15 +1,22 @@
 import SEO from "@/components/SEO";
 import CinematicCultureHome from "@/components/CinematicCultureHome";
+import ConsumerHomePreview from "@/pages/ConsumerHomePreview";
 import { useLayoutEffect } from "react";
 import { useI18n } from "@/i18n/I18nContext";
 
 const Index = () => {
   const { t } = useI18n();
+  const isConsumerPreview = new URLSearchParams(window.location.search).get("preview") === "consumer";
+
   useLayoutEffect(() => {
     if (!window.location.hash) {
       window.scrollTo({ top: 0, left: 0, behavior: "auto" });
     }
   }, []);
+
+  if (isConsumerPreview) {
+    return <ConsumerHomePreview />;
+  }
 
   return (
     <div className="min-h-screen">
