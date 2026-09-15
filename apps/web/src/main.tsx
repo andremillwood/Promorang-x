0f8f3d4d29514b0494234db0910a1de01163b385
andremillwood <andremillwood@gmail.com>
2026-09-14 20:17:15 -0500

    Load Promorang UI V2 foundation styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,6 +2,7 @@ import { I18nProvider } from "./i18n/I18nContext.tsx";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import "./styles/promorang-v2.css";
 
 import { HelmetProvider } from 'react-helmet-async';
 
