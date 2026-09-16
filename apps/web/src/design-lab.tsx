c880e4c53e48b2c60e11e7a341db920e79f50f57
andremillwood <andremillwood@gmail.com>
2026-09-16 00:27:20 -0500

    feat(design): render full design lab suite

--- apps/web/src/design-lab.tsx
+++ apps/web/src/design-lab.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import DesignLab from "./pages/DesignLab";
+import DesignLabSuite from "./pages/DesignLabSuite";
 
 const root = document.getElementById("design-lab-root");
 
@@ -11,6 +11,6 @@ if (!root) {
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <DesignLab />
+    <DesignLabSuite />
   </React.StrictMode>,
 );
