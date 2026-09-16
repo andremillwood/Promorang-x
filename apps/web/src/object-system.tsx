0383ea20ea418b1226492ec854bde1aa5b76f38d
andremillwood <andremillwood@gmail.com>
2026-09-16 15:14:58 -0500

    Wire canonical object shape grammar v2

--- apps/web/src/object-system.tsx
+++ apps/web/src/object-system.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from "react-router-dom";
 import CanonicalObjectSystemV1 from "@/pages/object-system/CanonicalObjectSystemV1";
 import "./index.css";
 import "./object-system.css";
+import "./object-system-shapes-v2.css";
 
 const rootElement = document.getElementById("object-system-root");
 if (!rootElement) throw new Error("Missing #object-system-root");
