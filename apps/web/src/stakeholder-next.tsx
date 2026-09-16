9aba8ab688dd53087f75259d138a08ee3d36340f
andremillwood <andremillwood@gmail.com>
2026-09-16 14:44:37 -0500

    Wire Stakeholder Next V6 object-embedded world

--- apps/web/src/stakeholder-next.tsx
+++ apps/web/src/stakeholder-next.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import StakeholderNextV5 from "@/pages/stakeholder-next/StakeholderNextV5";
+import StakeholderNextV6 from "@/pages/stakeholder-next/StakeholderNextV6";
 import "./index.css";
 import "./stakeholder-next-v2-fixes.css";
 
@@ -65,7 +65,7 @@ try {
   root.render(
     <React.StrictMode>
       <HashRouter>
-        <StakeholderNextV5 />
+        <StakeholderNextV6 />
       </HashRouter>
     </React.StrictMode>,
   );
